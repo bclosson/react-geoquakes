28f6e0cb614110c718a92ebcfbd4ac77edacc06c
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-11-20 13:46:51 -0800

    commit all quake info printing to page

--- src/components/QuakesComponent.js
+++ src/components/QuakesComponent.js
@@ -1,54 +1,73 @@
 import React from 'react';
+import ApiModel from '../models/ApiModel';
 
 class QuakesComponent extends React.Component {
     state = {
-      features:[{
-        type: "Feature",
-        properties: {
-          mag: 5.8,
-          place: "226 km SSW of Padang, Indonesia",
-          time: 1605577449575,
-          updated:1605578908697,
-          tz: null,
-          url: "https://earthquake.usgs.gov/earthquakes/eventpage/us7000cfxq",
-          detail: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us7000cfxq.geojson",
-          felt: null,
-          cdi: null,
-          mmi: 3.855,
-          alert: "green",
-          status: "reviewed",
-          tsunami: 0,
-          sig: 518,
-          net: "us",
-          code: "7000cfxq",
-          ids: ",us7000cfxq,",
-          sources: ",us,",
-          types: ",losspager,origin,phase-data,shakemap,",
-          nst: null,
-          dmin: 3.47,
-          rms:0.81,
-          gap:42,
-          magType: "mww",
-          type: "earthquake",
-          title: "M 5.8 - 226 km SSW of Padang, Indonesia"
-        },
-          geometry:{
-            type: "Point",
-            coordinates: [99.3071,-2.7016,10]
-          },
-        id: "us7000cfxq"
-          },
-      ],
+      features: [],
     }
-  
+    
+// dummy data: 
+// features:[{
+      //   type: "Feature",
+      //   properties: {
+      //     mag: 5.8,
+      //     place: "226 km SSW of Padang, Indonesia",
+      //     time: 1605577449575,
+      //     updated:1605578908697,
+      //     tz: null,
+      //     url: "https://earthquake.usgs.gov/earthquakes/eventpage/us7000cfxq",
+      //     detail: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us7000cfxq.geojson",
+      //     felt: null,
+      //     cdi: null,
+      //     mmi: 3.855,
+      //     alert: "green",
+      //     status: "reviewed",
+      //     tsunami: 0,
+      //     sig: 518,
+      //     net: "us",
+      //     code: "7000cfxq",
+      //     ids: ",us7000cfxq,",
+      //     sources: ",us,",
+      //     types: ",losspager,origin,phase-data,shakemap,",
+      //     nst: null,
+      //     dmin: 3.47,
+      //     rms:0.81,
+      //     gap:42,
+      //     magType: "mww",
+      //     type: "earthquake",
+      //     title: "M 5.8 - 226 km SSW of Padang, Indonesia"
+      //   },
+      //     geometry:{
+      //       type: "Point",
+      //       coordinates: [99.3071,-2.7016,10]
+      //     },
+      //   id: "us7000cfxq"
+      //     },
+      // ],
 
+
+    componentDidMount() {
+      this.fetchData();
+    };
+
+    fetchData = () => {
+      ApiModel.all().then((res) => {
+        this.setState({
+          features: res.data.features,
+        });
+      });
+    };
+   
   render() {
-    let eventTime = this.state.features[0].properties.time;
-    let currentTime = Date.now();
-    let hoursSinceEvent = (currentTime - eventTime) / 3600000;
-    console.log(this.state.features[0].properties.title);
-    console.log('diff of milliseconds =', currentTime - eventTime);
-    console.log("hours since event = ", Math.floor(hoursSinceEvent) );
+    const feature = this.state.features.map((quake) => {
+      let eventTime = quake.properties.time;
+      let currentTime = Date.now();
+      let hoursSinceEvent = (currentTime - eventTime) / 3600000;
+      let flooredHours = Math.floor(hoursSinceEvent);
+      return <p key={quake.id}>{quake.properties.title} {flooredHours} Hours Ago </p>;
+
+      });
+  
     return(
       <div>
 
@@ -57,7 +76,8 @@ class QuakesComponent extends React.Component {
         </div>
 
         <div id="info">
-          <p>{this.state.features[0].properties.title}</p>
+          { feature }
+          {/* { this.state.features[0].properties.title } */}
         </div>
 
       </div>
---
+++ src/models/ApiModel.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const endPoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
+
+class ApiModel {
+  static all = () => {
+    let request = axios.get(endPoint);
+    return request;
+  };
+};
+
+export default ApiModel;
